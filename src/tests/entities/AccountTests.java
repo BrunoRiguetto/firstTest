39fccfb7c57d9b8969bbd25e40362ccce3d59607
BrunoRiguetto <bruno.riguetto@gmail.com>
2021-09-24 09:29:37 -0300

    Tird test

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import entities.Account;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
 	
@@ -12,7 +13,7 @@ public class AccountTests {
 		
 		double amount = 200.0;
 		double expectedValue = 196.0;
-		Account acc = new Account(1L, 0.0);
+		Account acc = AccountFactory.creatEmptyAccount();
 		
 		acc.deposit(amount);
 		
@@ -23,12 +24,25 @@ public class AccountTests {
 	public void depositShouldDoNothingWhenNegativeAmount() {
 		
 		double expectedValue = 100.0;
-		Account acc = new Account(1L, expectedValue);
+		Account acc = AccountFactory.creatAccount(expectedValue);
 		double amount = -200.0;
 	
 		acc.deposit(amount);
 	
 		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+	
+	@Test
+	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+		
+		double expectedValue = 0.0;
+		double initialBalance = 800.0;
+		Account acc = AccountFactory.creatAccount(initialBalance);
+		
+		double result = acc.fullWithdrow();
+		
+		Assertions.assertTrue(expectedValue == acc.getBalance());
+		Assertions.assertTrue(result == initialBalance);
 	}
 	
 }
